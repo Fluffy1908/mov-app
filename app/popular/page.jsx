21cd5a89c2f531e839186337bce8294349de6d37
Fluffy1908 <pro1shinov.tima@gmail.com>
2023-11-21 22:25:21 -0500

    minor text changes

--- app/popular/page.jsx
+++ app/popular/page.jsx
@@ -4,7 +4,7 @@ export default function page() {
   
     return (
       <main>
-        <h1 id='popular-mov-text'>Popular Now</h1>
+        <h1 id='popular-mov-text'>Popular Movies Now</h1>
         <PopularFetch />
       </main>
   )
