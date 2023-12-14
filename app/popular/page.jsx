fe2310b063f27c95de4472a3ac31edb45dd3ebec
Fluffy1908 <pro1shinov.tima@gmail.com>
2023-12-14 11:07:42 -0500

    small fix

--- app/popular/page.jsx
+++ app/popular/page.jsx
@@ -5,7 +5,7 @@ export default function page() {
     <main>
       <h1 id="popular-mov-text">Popular Movies Now</h1>
       <h2 className="text-center text-2xl mt-5">
-        This page uses client-side rendering for better website SEO.
+        This page uses client-side rendering.
       </h2>
       <PopularFetch />
     </main>
